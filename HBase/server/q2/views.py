4e7257b6e368cd146e16a988ebd3693bb2ffc367
Tianyu Xu <xty213@gmail.com>
2014-10-24 01:11:03 -0400

    Python frontend bug fixed

--- HBase/server/q2/views.py
+++ HBase/server/q2/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 import happybase
 
 def handle_request(request):
-    conn = happybase.Connection('ec2-54-173-32-173.compute-1.amazonaws.com')
+    conn = happybase.Connection('ec2-54-173-66-253.compute-1.amazonaws.com')
     tweets = conn.table('tweets')
 
     if not 'userid' in request.GET or not request.GET['userid'] or \
@@ -11,13 +11,11 @@ def handle_request(request):
        return HttpResponse("")
 
     userid = request.GET['userid']
-    time = request.GET['tweet_time']
+    time = request.GET['tweet_time'].replace(' ', '+')
 
-    result = tweets.scan(filter="SingleColumnValueFilter('attr','time',=,'regexstring:^%s$') AND SingleColumnValueFilter('attr','user_id',=,'regexstring:^%s$')" % (time, userid))
+    data = tweets.row('%s %s' % (userid, time))
 
-    response = "ccteam, 6028-0384-4018, 4756-0270-5080, 1894-1779-2072\n"
+    # assume we will surely find at least one record if a userid and a time is given
+    response = "ccteam, 6028-0384-4018, 4756-0270-5080, 1894-1779-2072\n%s:%s:%s\n" % (data['attr:tweet_id'], data['attr:score'], data['attr:censored_text'])
 
-    for key, data in result:
-        response += "%s:%s:%s" % (data['attr:tweet_id'], data['attr:score'], data['attr:censored_text'])
-
-    return HttpResponse(response + "\n")
+    return HttpResponse(response)
