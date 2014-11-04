673d25b0c873bac90321de93c33cb5c5d5a11bf9
Haoliang Quan <hquan@andrew.cmu.edu>
2014-11-04 15:03:43 -0500

    Add q2 q3 q4 table

--- mysql/database_setup.sql
+++ mysql/database_setup.sql
@@ -4,4 +4,8 @@ CREATE DATABASE 15619project CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_c
 
 USE 15619project;
 
-CREATE TABLE tweets(tweet_id BIGINT PRIMARY KEY, censored_text VARCHAR(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci, score INT, time CHAR(19), user_id BIGINT);
+create table q2(tweet_id bigint primary key, user_id bigint, `time` char(19), `timestamp` bigint, score int, censored_text VARCHAR(640) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci) engine=myisam;
+
+create table q3(origin_user_id bigint, retweet_user_id bigint, primary key(origin_user_id, retweet_user_id)) engine=myisam;
+
+create table q4(tweet_id bigint primary key, hashed_location bigint, location VARCHAR(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci, date int(8), hashtag VARCHAR(640) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci, start_at int) engine=myisam;
