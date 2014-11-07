afe36c4fc89d0cf4a991cf606883d08101afec82
Tianyu Xu <xty213@gmail.com>
2014-11-07 00:17:58 -0500

    add new resin files and bug fixed

--- mysql/ROOT/src/Q2.java
+++ mysql/ROOT/src/Q2.java
@@ -1,8 +1,11 @@
 import java.io.IOException;
 import java.sql.Connection;
+import java.util.Date;
+import java.util.TimeZone;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletException;
@@ -13,61 +16,65 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 /**
- * Servlet implementation class Q1
+ * Servlet implementation class Q2
  */
-@WebServlet("/q1")
+@WebServlet("/q2")
 public class Q2 extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private static final String TEAM_ID = "\nccteam,6028-0384-4018,4756-0270-5080,1894-1779-2072\n";
-    private static final String QUERY = "SELECT tweet_id, score, censored_txt from q2 WHERE user_id = ? AND timestamp = ?";
+	private static final long serialVersionUID = 1L;
+	private static final String TEAM_ID = "ccteam,6028-0384-4018,4756-0270-5080,1894-1779-2072\n";
+	private static final String QUERY = "SELECT `tweet_id`, `score`, `censored_text` from q2 WHERE user_id=? AND timestamp=?";
 
-    @Resource(name = "jdbc/q3")
-    private DataSource q3Source;
+	@Resource(name = "jdbc/q3")
+	private DataSource q3Source;
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doGet(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/plain; charset=UTF-8");
-        response.setCharacterEncoding("UTF-8");
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/plain; charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
 
-        if (request.getParameter("userid") == null || request.getParameter("tweet_time") == null) {
-            response.getWriter().print("");
-            response.getWriter().flush();
-            return;
-        }
+		if (request.getParameter("userid") == null
+				|| request.getParameter("tweet_time") == null) {
+			response.getWriter().print("");
+			response.getWriter().flush();
+			return;
+		}
 
-        Connection conn = null;
-        try {
-            response.getWriter().print(TEAM_ID);
-            conn = q3Source.getConnection();
-            PreparedStatement ps = conn.prepareStatement(QUERY);
-            ps.setLong(1, Long.parseLong(request.getParameter("userid")));
+		Connection conn = null;
+		try {
+			response.getWriter().print(TEAM_ID);
+			conn = q3Source.getConnection();
+			PreparedStatement ps = conn.prepareStatement(QUERY);
+			ps.setLong(1, Long.parseLong(request.getParameter("userid")));
 
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            Date date = sdf.parse(request.getParameter("tweet_time"));
-            long timestamp = date.getTime() / 1000;
-            ps.setLong(2, timestamp);
-            ResultSet rs = ps.executeQuery();
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+			Date date = (Date) sdf.parse(request.getParameter("tweet_time"));
+			long timestamp = date.getTime() / 1000;
+			ps.setLong(2, timestamp);
+			ResultSet rs = ps.executeQuery();
 
-            while (rs.next()) {
-                response.getWriter().print(rs.getString(1) + ":" + rs.getString(2) + ":" + rs.getString(3) + "\n");
-            }
-            response.getWriter().flush();
-            rs.close();
-            ps.close();
+			while (rs.next()) {
+				response.getWriter().print(
+						rs.getString(1) + ":" + rs.getString(2) + ":"
+								+ rs.getString(3) + "\n");
+			}
+			response.getWriter().flush();
+			rs.close();
+			ps.close();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (conn != null)
-                    conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
